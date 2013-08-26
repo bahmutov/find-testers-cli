e8eebaa3968963b400bb14b9406fa4c72adf0cdb
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-08-26 08:12:34 -0400

    prompting user for country

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 var find = require('find-testers');
+var prompt = require('prompt');
+
+prompt.start();
+
+var inputs = [
+  {
+    name: 'country',
+    description: 'Enter desired countries (comma-separated)',
+    type: 'string',
+    message: 'countries',
+    default: 'ALL',
+    required: true
+  }
+];
+
+prompt.get(inputs, function (err, userValues) {
+  var testers = find({
+    country: userValues.country.split(',')
+  });
+  console.log('found', testers.length, 'testers:');
+  console.dir(testers);
+});
