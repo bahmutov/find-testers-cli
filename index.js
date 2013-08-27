8e457df5abaef62ca89a9cb82ddecc421dad6124
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-08-27 07:29:25 -0400

    added nice output table

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var find = require('find-testers');
 var prompt = require('prompt');
+var Table = require('easy-table');
+var check = require('check-types');
 
 prompt.start();
 
@@ -25,6 +27,23 @@ prompt.get(inputs, function (err, userValues) {
     country: userValues.country.split(','),
     device: userValues.device.split(',')
   });
-  console.log('found', testers.length, 'testers:');
-  console.dir(testers);
+  printTesters(testers);
 });
+
+function printTesters(testers) {
+  check.verifyArray(testers, 'expected list of testers');
+  console.log('found', testers.length, 'testers', '\n');
+  if (!testers.length) {
+    return;
+  }
+
+  var table = new Table();
+  testers.forEach(function (tester) {
+    table.cell('id', tester.testerId);
+    table.cell('first', tester.firstName);
+    table.cell('last', tester.lastName);
+    table.cell('bugs found', tester.bugs);
+    table.newRow();
+  });
+  console.log(table.toString());
+}
